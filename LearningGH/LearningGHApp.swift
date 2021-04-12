51bffc5179a879503182e0d38652af1855854136
Ryan Barkdull <rdbarkdull@gmail.com>
2021-04-12 12:54:06 -0500

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Ryan Barkdull on 4/12/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
